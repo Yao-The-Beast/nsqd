2f79ca0764195122cc73bcd829071992edb7c94c
Yao-The-Beast <drtailor@drtailors-mbp.dhcp.wustl.edu>
2016-05-17 15:52:52 -0500

    adsf

--- client_v2.go
+++ client_v2.go
@@ -446,7 +446,7 @@ func (c *clientV2) SetOutputBufferTimeout(desiredTimeout int) error {
 	
 	//yao
 	//hard code timeout for now
-	desiredTimeout = 10
+	desiredTimeout = 40
 	
 	switch {
 	case desiredTimeout == -1:
